6f7afc3734f4fcc4cc81dd4bf9d58c15914ad23b
KirdMNKY <mr.mike.soto@gmail.com>
2018-08-03 14:19:38 -0500

    first final merge

--- api.js
+++ api.js
@@ -1,78 +1,109 @@
-var keyword = prompt("What do you want to search on NYT?");
-var begin = prompt("Start year?");
-var end = prompt("End year?");
-
-begin += "0101";
-end += "1231";
 
 
-var numArticles = prompt("how many articles do you want?");
+// wait for page to load
+$(document).ready(function(){
+    var numRecords = $("#num-records");
+    var searchTerm = $("#search-term");
+    var startYear =  $("#start-year");
+    var endYear = $("#end-year");
+    var showArticles = $(".articles");
+    
+    var begin;
+    begin = "0101";
+    var end;
+    end = "1231";
+    
+    // On click event for search button
+    $(".btn-search").on("click", function(){
+        var searchData = searchTerm.val();
+        var numRecordsData = numRecords.val();
+        var startYearData = startYear.val()+begin;
+        var endYearData = endYear.val()+end;
 
-function createArticles(result, numWanted){
-    var ourDiv = $("<div>");
-    for(var i = 0; i < numWanted; i ++){
-        var articleDiv = $("<div>");
-        var articleTitle = $("<h4>");
-        articleTitle.text(result.response.docs[i].headline.main);
-        var author = $("<p>");
-        author.text(result.response.docs[i].byline.original);
-        var pubDate = $("<p>");
-        pubDate.text(result.response.docs[i].pub_date);
-        var url = $("<p>");
-        url.text(result.response.docs[i].web_url);
-        articleDiv.append(articleTitle);
-        articleDiv.append(author);
-        articleDiv.append(pubDate);
-        articleDiv.append(url);
-        $("#searchResults").append(articleDiv);
-    }
-}
-
-function getArticles(searchKeyword, begin_date, end_date, num_articles,){
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    var numWanted;
-    var remainder;
-    if(num_articles > 10){
-        numWanted = 10;
-        remainder = num_articles - 10;
-    }
-    else{
-        numWanted = num_articles;
-    }
-
-    url += '?' + $.param({
-        'api-key': "381fc0b99273425eaf1ebec4cb39ec8c",
-        'q': searchKeyword,
-        'begin_date': begin_date,
-        'end_date': end_date,
-        'page': 0,
+        getArticles(searchData, startYearData, endYearData, numRecordsData);
     });
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).then(function(result) {
-        // console.log(result);
-        createArticles(result, numWanted);
-    })
+    var numArticles = prompt("how many articles do you want?");
 
-    if(remainder > 0){
-        url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    function createArticles(result, numWanted){
+        var ourDiv = $("<div>");
+        for(var i = 0; i < numWanted; i ++){
+            var articleDiv = $("<div>");
+            var articleTitle = $("<h4>");
+            articleTitle.text(result.response.docs[i].headline.main);
+            var author = $("<p>");
+            author.text(result.response.docs[i].byline.original);
+            var pubDate = $("<p>");
+            pubDate.text(result.response.docs[i].pub_date);
+            var url = $("<p>");
+            url.text(result.response.docs[i].web_url);
+            articleDiv.append(articleTitle);
+            articleDiv.append(author);
+            articleDiv.append(pubDate);
+            articleDiv.append(url);
+            showArticles.append(articleDiv);
+        }
+    }
+
+    function getArticles(searchKeyword, begin_date, end_date, num_articles,){
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        var numWanted;
+        var remainder;
+        if(num_articles > 10){
+            numWanted = 10;
+            remainder = num_articles - 10;
+        }
+        else{
+            numWanted = num_articles;
+        }
+
         url += '?' + $.param({
             'api-key': "381fc0b99273425eaf1ebec4cb39ec8c",
             'q': searchKeyword,
             'begin_date': begin_date,
             'end_date': end_date,
-            'page': 1,
+            'page': 0,
         });
         $.ajax({
             url: url,
             method: 'GET',
         }).then(function(result) {
             // console.log(result);
-            createArticles(result,remainder);
+            createArticles(result, numWanted);
         })
+
+        if(remainder > 0){
+            url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+            url += '?' + $.param({
+                'api-key': "381fc0b99273425eaf1ebec4cb39ec8c",
+                'q': searchKeyword,
+                'begin_date': begin_date,
+                'end_date': end_date,
+                'page': 1,
+            });
+            $.ajax({
+                url: url,
+                method: 'GET',
+            }).then(function(result) {
+                // console.log(result);
+                createArticles(result,remainder);
+            })
+        }
     }
-}
 
+     // On click event for clear button
+     $(".btn-clear").on("click", function(){
+        // calling reset function
+        reset();
+    });
 
-getArticles(keyword, begin, end, numArticles);
+    function reset(){
+        // user text = null
+        numRecords.val(5);
+        searchTerm.val("");
+        startYear.val("");
+        endYear.val("");
+        $(".articles").empty("");
+    
+    }
+
+});
